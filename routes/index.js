076ee52af5d03caa05cdd151a2471078e167db00
alicbm <abarandica1234@gmail.com>
2023-02-14 21:48:41 -0500

    rutas comantadas, ultimo intento de hoy

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,16 @@
 const express = require('express')
 
-const productRouter = require('./product.router')
+// const productRouter = require('./product.router')
 const categoryRouter = require('./category.router')
-const feedbackRouter = require('./feedback.router')
+// const feedbackRouter = require('./feedback.router')
 
 function routerApi (app){
   const router = express.Router();
   app.use('/api/v1', router);
 
-  router.use('/products', productRouter)
+  // router.use('/products', productRouter)
   router.use('/categories', categoryRouter)
-  router.use('/feedback', feedbackRouter)
+  // router.use('/feedback', feedbackRouter)
 }
 
 module.exports = routerApi;
