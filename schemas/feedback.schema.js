3085278c6de5dcc42d402507f800e987510e3632
alicbm <abarandica1234@gmail.com>
2023-03-12 19:26:47 -0500

    feedbackModel modified, added bad, regular and good options

--- schemas/feedback.schema.js
+++ schemas/feedback.schema.js
@@ -2,8 +2,11 @@ const Joi = require('joi')
 
 const id = Joi.number().integer();
 const name = Joi.string();
-const description = Joi.string();
+const bad = Joi.string();
+const regular = Joi.string();
+const good = Joi.string();
 const createAt = Joi.string();
+const description = Joi.string();
 const productId = Joi.number().integer();
 
 const getFeedbackSchema = Joi.object({
@@ -13,6 +16,9 @@ const getFeedbackSchema = Joi.object({
 const createFeedbackSchema = Joi.object({
   id: id.required(),
   name: name.required(),
+  bad: bad.required(),
+  regular: regular.required(),
+  good: good.required(),
   description: description.required(),
   createAt: createAt.required(),
   productId: productId.required()
@@ -21,6 +27,9 @@ const createFeedbackSchema = Joi.object({
 const updateFeedbackSchema = Joi.object({
   id: id.optional(),
   name: name.optional(),
+  bad: bad.optional(),
+  regular: regular.optional(),
+  good: good.optional(),
   description: description.optional(),
   createAt: createAt.optional(),
   productId: productId.optional()
