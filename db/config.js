92d1ea47591afdd56cddb8ba00a92ed34ab48061
alicbm <abarandica1234@gmail.com>
2023-04-03 10:19:39 -0500

    url of database modified

--- db/config.js
+++ db/config.js
@@ -1,4 +1,4 @@
-const URI = 'mysql://root:kP9UmpxUt5lRHLF7WHuQ@containers-us-west-23.railway.app:6970/railway'
+const URI = 'mysql://root:wVivQmXba5zExdzGzmmp@containers-us-west-23.railway.app:6970/railway'
 
 module.exports = {
   development: {
--- libs/sequelize.js
+++ libs/sequelize.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize');
 
 const setupModels = require('../db/models')
 
-const URI = 'mysql://root:kP9UmpxUt5lRHLF7WHuQ@containers-us-west-23.railway.app:6970/railway'
+const URI = 'mysql://root:wVivQmXba5zExdzGzmmp@containers-us-west-23.railway.app:6970/railway'
 
 const sequelize = new Sequelize(URI, {
   dialect: 'mysql',
